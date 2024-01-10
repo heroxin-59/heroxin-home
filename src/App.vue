ce9808b636517c01a3676345bf098d0df4aafb06
Heroxin <hero_xin59@163.com>
2024-01-10 15:54:51 +0800

    取消banner

--- src/App.vue
+++ src/App.vue
@@ -108,13 +108,7 @@ onMounted(() => {
   const styleTitle2 = "font-size:12px;color: rgb(244,167,89);";
   const styleContent = "color: rgb(30,152,255);";
   const title1 = "Heroxinの主页";
-  const title2 = `
- ____  ____                                 _
-|_   ||   _|                               (_)
-  | |__| |  .---.  _ .--.   .--.   _   __  __   _ .--.
-  |  __  | / /__\\[ `/'`\]/ .'`\ \[ \ [  ][  | [ `.-. |
- _| |  | |_| \__., | |    | \__. | > '  <  | |  | | | |
-|____||____|'.__.'[___]    '.__.' [__]`\_][___][___||__]`;
+  const title2 = `banner`;
   const content = `\n\n版本: ${config.version}\n主页: ${config.home}\nGithub: ${config.github}`;
   console.info(`%c${title1} %c${title2} %c${content}`, styleTitle1, styleTitle2, styleContent);
 });
